152f1f2c21114172d7d911ec02fa2a451b58c599
muharib-0 <mohammedfayazhussain13@gmail.com>
2025-12-18 17:01:36 +0530

    modified config for loading configurations from enviromental variables

--- config/config.go
+++ config/config.go
@@ -1,11 +1,39 @@
 package config
 
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 // Config struct to hold configuration variables
 type Config struct {
-	DBSource string
+	ServerPort  string
+	DatabaseURL string
+	DBDriver    string
 }
 
+// LoadConfig loads configuration from environment variables
 func LoadConfig(path string) (config Config, err error) {
-	// TODO: Implement config loading (e.g. from env or file)
-	return
+	err = godotenv.Load(path + "/.env")
+	if err != nil {
+		// If .env file doesn't exist, try to use environment variables directly
+		err = nil
+	}
+
+	config = Config{
+		ServerPort:  getEnv("SERVER_PORT", "3000"),
+		DatabaseURL: getEnv("DATABASE_URL", ""),
+		DBDriver:    getEnv("DB_DRIVER", "postgres"),
+	}
+
+	return config, nil
+}
+
+// getEnv gets an environment variable or returns a default value
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
 }
